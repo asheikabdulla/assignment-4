d05eddfb9b3c73bb6526e27de77b89d3bc3419ae
asheikabdulla <91787538+asheikabdulla@users.noreply.github.com>
2021-10-24 09:14:50 -0700

    Add files via upload

---
+++ find the way with maps.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+sample_list = [1,2,3,4,5,6,7]
+triple = list(map(lambda x: x*3,sample_list))
+print('triple of list numbers: ',triple)
+
+
+# In[ ]:
+
+
+
+

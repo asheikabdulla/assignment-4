f9ca6d4c208da1d361267206a770c6040fc541b6
asheikabdulla <91787538+asheikabdulla@users.noreply.github.com>
2021-10-24 09:15:12 -0700

    Add files via upload

---
+++ find the square from given list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+sample_list = [4,5,2,9]
+squared_list = list(map(lambda x: x**2,sample_list))
+print('square the elements of the list: ',squared_list)
+
+
+# In[ ]:
+
+
+
+
